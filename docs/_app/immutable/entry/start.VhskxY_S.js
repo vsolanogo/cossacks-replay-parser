ac2030b2ceaf5a4a5957a23264c7f40dae32d3a7
vsolanogo <skreeauk@gmail.com>
2025-11-13 02:50:20 +0200

    svelte

--- docs/_app/immutable/entry/start.VhskxY_S.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/cPfd2kAc.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,9 @@ const config = {
       precompress: false,
       strict: true
     }),
-    paths: {
-      base: '/cossacks-replay-parser'
-    }
+    // paths: {
+    //   base: '/cossacks-replay-parser/'
+    // }
   }
 };
 
