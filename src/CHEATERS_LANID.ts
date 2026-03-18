3019e20a486cf74148ea95ab887eb5adddac73fc
vsolanogo <skreeauk@gmail.com>
2026-03-18 15:37:41 +0200

    Add comments for double upgrades abusers in CHEATERS_LANID

--- src/CHEATERS_LANID.ts
+++ src/CHEATERS_LANID.ts
@@ -1,10 +1,11 @@
 export const CHEATERS_LANID = [
     199259, /*Payton_3432*/
     287176, /*Gamja*/
-    364459, /*KOPCAP*/
+    364459, /*KOPCAP double upgrades abuser*/
     351554, /*Left*/
     318040, /*Luc1k*/
     336746, /*Titus*/
     303090, /*bazuka bratan etc.*/
     359812, /*BorodachLena*/
+    377326, /*juan pedro - double upgrades abuser*/
 ];
